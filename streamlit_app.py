78a549f1ab4b27e5f1bb174dee5318edb9c34378
noviksv <58445455+noviksv@users.noreply.github.com>
2025-10-20 14:46:12 +0000

    Refactor data handling to convert Snowflake DataFrame to Pandas DataFrame for smoother processing

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,7 +3,7 @@ import streamlit as st
 #from snowflake.snowpark.context import get_active_session
 from snowflake.snowpark.functions import col
 import requests
-
+import pandas as pd
 
 # Write directly to the app
 st.title(f":cup_with_straw: Customize your Smoothie:cup_with_straw:")
@@ -20,8 +20,12 @@ st.write(f"the name on your smoothie will be:{name_on_order}")
 cnx = st.connection("snowflake")
 session = cnx.session()
 my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'),col('SEARCH_ON'))
-st.dataframe(data=my_dataframe, use_container_width=True)
-st.stop()
+# st.dataframe(data=my_dataframe, use_container_width=True)
+# st.stop()
+
+pd_df = my_dataframe.to_pandas()
+st.dataframe(data=pd_df)
+
 
 ingredents_list = st.multiselect("choose up to 5 ingridients:", my_dataframe,
                      max_selections=6)
@@ -30,6 +34,10 @@ if ingredents_list:
 
     for fruit_chosen in ingredents_list:
         ingredents_string += fruit_chosen + ' '
+
+        search_on=pd_df.loc[pd_df['FRUIT_NAME'] == fruit_chosen, 'SEARCH_ON'].iloc[0]
+        st.write('The search value for ', fruit_chosen,' is ', search_on, '.')
+
         st.subheader(f"{fruit_chosen} Nutrition Information")
         smoothiefroot_response = requests.get(f"https://my.smoothiefroot.com/api/fruit/{fruit_chosen}")
         sf_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width=True)
