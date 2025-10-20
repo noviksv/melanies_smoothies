63b0039ad9f8964acf037a1dae25c56284a1d6eb
noviksv <58445455+noviksv@users.noreply.github.com>
2025-10-20 14:17:18 +0000

    Reorder smoothie API response handling in Streamlit app

--- streamlit_app.py
+++ streamlit_app.py
@@ -12,8 +12,7 @@ st.write(
   """
 )
 
-smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
-st.text(smoothiefroot_response)
+
 
 name_on_order = st.text_input("Name on Smoothie")
 st.write(f"the name on your smoothie will be:{name_on_order}")
@@ -45,3 +44,6 @@ if ingredents_list:
         if ingredents_string:
             session.sql(my_insert_stmt).collect()
             st.success(f'Your Smoothie is ordered, {name_on_order}!', icon="✅")
+
+smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
+sf_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width=True)
